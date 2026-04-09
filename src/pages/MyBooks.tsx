0f4ff7c6fe47dd3e78a929e91c19ea2bd04c7574
Radek Kaluža <kaluza.radek@gmail.com>
2026-04-09 19:16:20 +0200

    feat: gql codegen & others

--- src/pages/MyBooks.tsx
+++ src/pages/MyBooks.tsx
@@ -1,9 +1,9 @@
+import { useQuery } from '@apollo/client/react'
 import { useSnackbar } from 'notistack'
 import { useMemo, useState } from 'react'
 import { BookList } from '../components/BookList/BookList'
 import { SearchBar } from '../components/SearchBar/SearchBar'
 import { useDeleteBookMutation } from '../services/mutations/useDeleteBookMutation'
-import { useBooksQuery } from '../services/queries/useBooksQuery'
 import { Mode } from '../utils/types'
 
 export const MyBooks = () => {
@@ -11,7 +11,7 @@ export const MyBooks = () => {
 
   const { enqueueSnackbar } = useSnackbar()
 
-  const { data: books } = useBooksQuery()
+  const { data: books } = useQuery(MyBooksDocument)
   const { mutate: deleteBook, isPending: isDeletingBook } = useDeleteBookMutation()
 
   const filteredBooks = useMemo(() => {
