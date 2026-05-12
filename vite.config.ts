b8903d37a71590fcfbb53a207ef08e917d941585
Radek Kaluža <kaluza.radek@gmail.com>
2026-05-12 17:05:36 +0200

    feat: add HTTPS + certs into vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,22 @@
-import basicSsl from '@vitejs/plugin-basic-ssl'
+import fs from 'node:fs'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [react(), basicSsl()],
+  plugins: [react()],
+  server: {
+    host: true,
+    https: {
+      key: fs.readFileSync('./temp/server.key'),
+      cert: fs.readFileSync('./temp/server.crt'),
+    },
+  },
   preview: {
+    host: true,
+    https: {
+      key: fs.readFileSync('./temp/server.key'),
+      cert: fs.readFileSync('./temp/server.crt'),
+    },
     allowedHosts: ['azul-nas.local', '192.168.0.98', 'localhost'],
   },
 })
