2f45281416ecbd4ffc2eacc1560fae8c31ab4774
Radek Kaluža <kaluza.radek@gmail.com>
2026-06-03 11:58:24 +0200

    build: read VITE_GOOGLE_BOOKS_API_KEY from FE/.env and remove it from root .env

--- deploy.sh
+++ deploy.sh
@@ -26,7 +26,7 @@ TAG="${TAG:-latest}"
 MODE="${MODE:-ssh}"
 BUILD_ONLY="${BUILD_ONLY:-false}"
 HTTPS_PORT="${HTTPS_PORT:-9443}"
-VITE_GOOGLE_BOOKS_API_KEY="${VITE_GOOGLE_BOOKS_API_KEY:-}"
+VITE_GOOGLE_BOOKS_API_KEY="${VITE_GOOGLE_BOOKS_API_KEY:-$(grep '^VITE_GOOGLE_BOOKS_API_KEY=' FE/.env 2>/dev/null | cut -d= -f2-)}"
 
 # Synology DSM does not add docker to PATH for SSH sessions
 NAS_DOCKER="${NAS_DOCKER:-/var/packages/ContainerManager/target/usr/bin/docker}"
